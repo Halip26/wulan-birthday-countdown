021c4efaf3eec3837827d65943c0c9313740896c
Halip26 <77138807+Halip26@users.noreply.github.com>
2024-11-07 22:04:06 +0800

    change again with add main url

--- assets/js/script.js
+++ assets/js/script.js
@@ -110,10 +110,10 @@ document.getElementById("currentYear").textContent = currentYear;
 
 // Array of background images
 const images = [
-  'url("../assets/images/triwulandari11_sunset.jpg")',
-  'url("../assets/images/triwulandarii11_ootd.jpg")',
-  'url("../assets/images/triwulandarii11_ootd2.jpg")',
-  'url("../assets/images/triwulandarii11_casual.jpg")',
+  'url("https://halip26.github.io/wulan-birthday-countdown/assets/images/triwulandari11_sunset.jpg")',
+  'url("https://halip26.github.io/wulan-birthday-countdown/assets/images/triwulandarii11_ootd.jpg")',
+  'url("https://halip26.github.io/wulan-birthday-countdown/assets/images/triwulandarii11_ootd2.jpg")',
+  'url("https://halip26.github.io/wulan-birthday-countdown/assets/images/triwulandarii11_casual.jpg")',
 ];
 
 // Initial index
