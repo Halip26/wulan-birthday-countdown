6ffb726435980f25d977c0ebb990a1394d80cf3b
Halip26 <77138807+Halip26@users.noreply.github.com>
2024-11-11 21:57:35 +0800

    add new button to click

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,7 +36,7 @@ let tempMonth = tempDate.getMonth();
 let tempDay = tempDate.getDate();
 
 // Set the date of my birthday
-const futureBirthday = new Date(2024, 10, 11, 22, 30, 0);
+const futureBirthday = new Date(2024, 10, 11, 18, 30, 0);
 
 const year = futureBirthday.getFullYear();
 const hours = futureBirthday.getHours();
@@ -92,7 +92,7 @@ function getRemindingTime() {
 
   if (t < 0) {
     clearInterval(countdown);
-    timeLeft.innerHTML = `<h4 class="expired">Sorry, this birthday date has passed!</h4>`;
+    timeLeft.innerHTML = `<h4 class="expired">Sorry, this birthday has passed!  <br /> Click the button below 👇 <br /> <a href="birthday-card.html">Birthday wishes </a> </h4>`;
   }
 }
 
